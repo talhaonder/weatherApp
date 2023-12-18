d8598948673fd4d293053d9f1fe96d2686a6cd46
talhaonder <ltalhaonderl@gmail.com>
2023-12-18 14:55:07 +0300

    bg-blur-downlaod-createNativeNavigator-and-others

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,7 @@ export default function HomeScreen() {
     return (
         <View style={{ flex: 1, position: 'relative' }}>
             <StatusBar />
-            <Image source={require('../assets/image/avatar.png')}
+            <Image blurRadius={70} source={require('../assets/image/avatar.png')}
                 style={{ position: 'absolute', height: '100%', width: '100%' }} />
         </View>
     )
