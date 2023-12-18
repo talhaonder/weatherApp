5bb251336ce7afd0c9860b225b2de6e5e2bb8216
talhaonder <ltalhaonderl@gmail.com>
2023-12-18 15:24:13 +0300

    added-search-bar-and-dark-theme-on-search-bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,21 @@
-import { View, Text, SafeAreaView, StatusBar, Image } from "react-native";
+import { View, Text, SafeAreaView, StatusBar, Image, TextInput, TouchableOpacity } from "react-native";
 import React from "react";
 import { theme } from "../theme";
+
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
+
 
 export default function HomeScreen() {
     return (
         <View style={{ flex: 1, position: 'relative' }}>
             <StatusBar />
-            <Image blurRadius={70} source={require('../assets/image/avatar.png')}
+            <Image blurRadius={50} source={require('../assets/image/bg.png')}
                 style={{ position: 'absolute', height: '100%', width: '100%' }} />
             <SafeAreaView
             style={{flex: 1}}
             >
                 <View
-                style={{height: "7%", position: "relative"}}
+                style={{height: "7%", position: "relative", padding: 12,}}
                 >
                     <View
                     style={{ flexDirection: 'row',
@@ -20,7 +23,15 @@ export default function HomeScreen() {
                     alignItems: 'center',
                     borderRadius: 999,
                     backgroundColor: theme.bgWhite(0.2)  }}>
-
+                        <TextInput
+                            placeholder="Search City"
+                            placeholderTextColor={'lightgray'}
+                            style={{ flex: 1, fontSize: 16, color: 'white', paddingLeft:16, margin:4, }}
+                        />
+                        <TouchableOpacity
+                        style={{ backgroundColor: theme.bgWhite(0.3), borderRadius: 999, padding: 12, margin: 4 }}>
+                        <MagnifyingGlassIcon size="25" color= "white"  />
+                        </TouchableOpacity>
                     </View>
                 </View>
             </SafeAreaView>
