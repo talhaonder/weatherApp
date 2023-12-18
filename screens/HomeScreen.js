d683ba33b3cbeb710107fd127da76b9388b1a612
talhaonder <ltalhaonderl@gmail.com>
2023-12-18 15:03:06 +0300

    add-opacity-on-theme

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import { View, Text, SafeAreaView, StatusBar, Image } from "react-native";
 import React from "react";
+import { theme } from "../theme";
 
 export default function HomeScreen() {
     return (
@@ -7,6 +8,22 @@ export default function HomeScreen() {
             <StatusBar />
             <Image blurRadius={70} source={require('../assets/image/avatar.png')}
                 style={{ position: 'absolute', height: '100%', width: '100%' }} />
+            <SafeAreaView
+            style={{flex: 1}}
+            >
+                <View
+                style={{height: "7%", position: "relative"}}
+                >
+                    <View
+                    style={{ flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                    alignItems: 'center',
+                    borderRadius: 999,
+                    backgroundColor: theme.bgWhite(0.2)  }}>
+
+                    </View>
+                </View>
+            </SafeAreaView>
         </View>
     )
 }
